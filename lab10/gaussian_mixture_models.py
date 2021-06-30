edeae0be5b4dd1f325c2bd211cc0887e4d9dd50b
giuseppesalvi <giuseppe.salvi98@gmail.com>
2021-06-30 11:55:30 +0200

    feat: M-step

--- lab10/gaussian_mixture_models.py
+++ lab10/gaussian_mixture_models.py
@@ -4,6 +4,7 @@ import scipy.special as sps
 from data.GMM_load import load_gmm
 from modules.gaussian_density_estimation import logpdf_GAU_ND
 
+
 def vcol(x):
     """ reshape the vector x into a column vector """
 
@@ -41,8 +42,8 @@ def logpdf_GMM(X, gmm):
             # gmm[g][1] = mu_g, gmm[g][2] = C_g
             S[g, j] = logpdf_GAU_ND(sample, vcol(gmm[g][1]), gmm[g][2])
 
-    # Add to each row of S the logarithm of the prior of the corresponding 
-    # component log w_g 
+    # Add to each row of S the logarithm of the prior of the corresponding
+    # component log w_g
     for g in range(len(gmm)):
         # gmm[g][0] = w_g
         S[g, :] += np.log(gmm[g][0])
@@ -56,7 +57,7 @@ def EM_algorithm(X, initial_gmm):
     """ Implementation of the GMM EM estimation procediure:
         the EM algorithm is useful to estimate the parameters of a GMM
         that maximize the likelihood for traning set X
-        The initial estimate of the GMM is passes as parameter
+        The initial estimate of the GMM is passed as parameter
     """
 
     gmm = initial_gmm
@@ -74,20 +75,20 @@ def EM_algorithm(X, initial_gmm):
         # component Gi = g for all samples xi
         S = np.empty([M, N])
 
-        for g in range(len(gmm)):
+        for g in range(M):  # for g in range(len(gmm)):
             for j, sample in enumerate(X.T):
                 sample = vcol(sample)
                 # gmm[g][1] = mu_g, gmm[g][2] = C_g
                 S[g, j] = logpdf_GAU_ND(sample, vcol(gmm[g][1]), gmm[g][2])
 
-        # Add to each row of S the logarithm of the prior of the corresponding 
-        # component log w_g 
-        for g in range(len(gmm)):
+        # Add to each row of S the logarithm of the prior of the corresponding
+        # component log w_g
+        for g in range(M):  # for g in range(len(gmm)):
             # gmm[g][0] = w_g
             S[g, :] += np.log(gmm[g][0])
 
         # S is now the matrix of joint densities f_Xi,Gi(xi,g)
-        
+
         # Compute the log-marginal log f_Xi(xi) for all samples xi
         logdens = sps.logsumexp(S, axis=0)
 
@@ -95,32 +96,46 @@ def EM_algorithm(X, initial_gmm):
         # containing the N marginal densities logdens
         log_responsabilities = S - logdens
 
-        # Compute the MxN matrix of posterior probabilities, responsabilities
+        # Compute the MxN matrix of class posterior probabilities, responsabilities
         responsabilities = np.exp(log_responsabilities)
 
         # M-step
 
         # Compute statistics
-        Zg = responsabilities.sum(axis=1)
+        Zg_list = []
+        Fg_list = []
+        Sg_list = []
+        for g in range(M):
+            Zg_list.append(vrow(responsabilities[g]).sum(axis=1))
+            Fg_list.append((vrow(responsabilities[g]) * X).sum(axis=1))
+            tmp = np.zeros((4, 4))
+            for i in range(N):
+                tmp += responsabilities[g][i] * np.dot(vcol(X.T[i]), vrow(X.T[i]))
+            Sg_list.append(tmp)
+
+        ##Zg = responsabilities.sum(axis=1)
         # Fg = (np.dot(responsabilities, X.T)).sum(axis=1)
         # Sg = (np.dot(np.dot(responsabilities, X.T), X)).sum(axis=1)
 
         # Obtain the new paramters
-        # mu_new = Fg / Zg
-        # sigma_new = (Sg / Zg) - np.dot(mu_new, mu_new.T)
-        # w_new = Zg / (Zg.sum())
+        for g in range(M):
+            w_new = (Zg_list[g] / sum(Zg_list))[0]  # extract the float
+            mu_new = vcol(Fg_list[g] / Zg_list[g])
+            sigma_new = (Sg_list[g] / Zg_list[g]) - np.dot(vcol(mu_new), vrow(mu_new))
+            gmm[g]= (w_new, mu_new, sigma_new)
 
+        # Check stopping criterion TODO
     return 0
 
 
 if __name__ == "__main__":
 
     # Gaussian mixture models
-    
-    # Load data 
+
+    # Load data
     X = np.load("data/GMM_data_4D.npy")
 
-    # Load the reference GMM 
+    # Load the reference GMM
     gmm = load_gmm("data/GMM_4D_3G_init.json")
 
     # Solution log densities for all samples in X
@@ -130,7 +145,7 @@ if __name__ == "__main__":
     # print(sol_logdens)
 
     # Check if my algorithm is correct
-    log_dens = logpdf_GMM(X, gmm) 
+    log_dens = logpdf_GMM(X, gmm)
 
     # print("My results:")
     # print(log_dens)
@@ -139,7 +154,6 @@ if __name__ == "__main__":
     if (sol_logdens - log_dens).sum():
         print("Error: algorithm of GMM does not work")
 
-
     # GMM estimation: the EM algorithm
 
-    avg_ll = EM_algorithm(X, gmm) 
+    avg_ll = EM_algorithm(X, gmm)
